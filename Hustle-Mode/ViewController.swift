b27f155a35b2d9aac725a2210bb4481b5572489c
Andrew Colbeck <andrewcolbeck@gmail.com>
2019-01-29 20:55:43 +1100

    Converted to iPhone X, working

--- Hustle-Mode/ViewController.swift
+++ Hustle-Mode/ViewController.swift
@@ -43,7 +43,7 @@ class ViewController: UIViewController {
         player.play()
         
         UIView.animate(withDuration: 2.3, animations: {
-            self.rocket.frame = CGRect(x: 0, y: 140, width: 375, height: 402)
+            self.rocket.frame = CGRect(x: 0, y: 180, width: 375, height: 402)
         }) { (finished) in
             self.hustleLbl.isHidden = false
             self.onLbl.isHidden = false
