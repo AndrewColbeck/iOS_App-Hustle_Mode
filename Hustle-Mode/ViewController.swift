729d4cc5585a06cafa68ce0078096daef3479bab
Andrew Colbeck <andrewcolbeck@gmail.com>
2019-01-29 17:15:19 +1100

    Created Project

---
+++ Hustle-Mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hustle-Mode
+//
+//  Created by andru on 29/1/19.
+//  Copyright © 2019 andrew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
